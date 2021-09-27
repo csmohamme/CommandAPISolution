4eecc3ac9342274eeb1b1812093d63501134f355
Mohammed Almostafa <cs.mohammedalmostafa@outlook.com>
2021-09-27 09:27:15 +0200

    Added Model & MockRepo in our CommandController

--- src/CommandAPI/Controllers/CommandsCOntroller.cs
+++ src/CommandAPI/Controllers/CommandsCOntroller.cs
@@ -1,14 +1,34 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
+
 namespace CommandAPI.Contrllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class CommandsController : ControllerBase
     {
+        private readonly ICommandAPIRepo _respository;
+        public CommandsController(ICommandAPIRepo repository)
+        {
+            _respository = repository;
+        }
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get(){
-            return new string[] {"this","is","hard","coded"};
+        public ActionResult<IEnumerable<Command>> GetAllCommands(){
+            var commandItems = _respository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id){
+            var commandItem = _respository.GetCommandById(id);
+            if(commandItem == null){
+                return NotFound();
+            }
+            return Ok(commandItem);
         }
     }
 }
